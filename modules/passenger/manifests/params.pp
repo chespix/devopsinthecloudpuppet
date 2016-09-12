cf67ff99c60c6b74076b9cb64e8721e3dd4eee36
Chespix <chespi@gmail.com>
2016-09-12 10:08:56 -0300

    update ruby version to 2.0 in passger module parameters.

--- modules/passenger/manifests/params.pp
+++ modules/passenger/manifests/params.pp
@@ -3,7 +3,7 @@
 class passenger::params {
   $version='3.0.19'
 
-  $gem_path = '/usr/lib/ruby/gems/1.8/gems'
-  $gem_binary_path = '/usr/lib/ruby/gems/1.8/gems/bin'
-  $mod_passenger_location = "/usr/lib/ruby/gems/1.8/gems/passenger-$version/ext/apache2/mod_passenger.so"
+  $gem_path = '/usr/lib/ruby/gems/2.0/gems'
+  $gem_binary_path = '/usr/lib/ruby/gems/2.0/gems/bin'
+  $mod_passenger_location = "/usr/lib/ruby/gems/2.0/gems/passenger-$version/ext/apache2/mod_passenger.so"
 }
